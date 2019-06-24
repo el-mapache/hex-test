14b1b8abda4a61ca26ec482e511bdf986963d7a0
Adam B <el-mapache@users.noreply.github.com>
2019-06-24 11:06:04 -0700

    Experiment to only show hex on unmask

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const SVG = require("svg.js");
 const honeycomb = require("honeycomb-grid");
 
 // Initialize the SVG container element
-const photoHex2 = SVG(document.getElementById("photo-hex-2"));
+const imageContainer = SVG(document.getElementById("svg-canvas"));
 
 // create a hexagon factory
 const Hex = honeycomb.extendHex({ size: 20 });
@@ -14,7 +14,7 @@ const corners = Hex().corners();
 
 // in-memory representation of a grid of hexagons, from which the
 // concrete hex grid will be rendered
-const backingGrid = Grid.rectangle({ width: 22, height: 22 });
+const backingGrid = Grid.rectangle({ width: 46, height: 22 });
 const IMAGE_OFFSET = 22;
 
 SVG.Hexagon = SVG.invent({
@@ -22,8 +22,8 @@ SVG.Hexagon = SVG.invent({
   create() {
     return new SVG.Defs()
       .polygon(corners.map(({ x, y }) => `${x},${y}`))
-      .fill({ color: "#fff", opacity: "0" })
-      .stroke({ width: "1", color: "#000", opacity: "0.9" });
+      .fill({ color: "#fff", opacity: "0" });
+    //.stroke({ width: "1", color: "#000", opacity: "0.1" });
   },
   construct: {
     hexagon(x, y) {
@@ -34,10 +34,10 @@ SVG.Hexagon = SVG.invent({
 
 SVG.HexagonGroup = SVG.invent({
   inherit: SVG.G,
-  create() {
+  create(grid) {
     const group = new SVG.G();
 
-    backingGrid.forEach(hex => {
+    grid.forEach(hex => {
       const { x, y } = hex.toPoint();
       group.add(this.hexagon(x, y));
     });
@@ -45,14 +45,13 @@ SVG.HexagonGroup = SVG.invent({
     return group;
   },
   construct: {
-    hexagonGroup() {
-      return this.put(new SVG.HexagonGroup());
+    hexagonGroup(backingHexagons) {
+      return this.put(new SVG.HexagonGroup(backingHexagons));
     }
   }
 });
 
 const paintImage = ({ svg, url, offset, size }) => {
-  console.log(size);
   const image = size
     ? svg.image(url, size.width, size.height)
     : svg.image(url).loaded(function(imgData) {
@@ -66,31 +65,24 @@ const paintImage = ({ svg, url, offset, size }) => {
   return image;
 };
 
-const hexagons = photoHex2.hexagonGroup();
+const hexagons = imageContainer.hexagonGroup(backingGrid);
 
 // paint the initial image
-paintImage({
-  svg: photoHex2,
-  url: "./src/img/place-cat-a.jpeg",
-  offset: { x: IMAGE_OFFSET, y: IMAGE_OFFSET }
+const initialImage = paintImage({
+  svg: imageContainer,
+  url: "./src/img/series/series1-notes.png",
+  offset: { x: IMAGE_OFFSET, y: IMAGE_OFFSET },
+  size: { width: 600, height: 200 }
 });
 
-const catImageB = photoHex2
-  .image("./src/img/place-cat-b.jpeg", 400, 200)
-  .translate(IMAGE_OFFSET, IMAGE_OFFSET);
-// paintImage({
-//   svg: photoHex2,
-//   url: "./src/img/place-cat-b.jpeg",
-//   offset: { x: 22, y: 22 }
-// });
+const maskingImage = paintImage({
+  svg: imageContainer,
+  url: "./src/img/phrases/series1.png",
+  offset: { x: IMAGE_OFFSET, y: IMAGE_OFFSET },
+  size: { width: 600, height: 200 }
+});
 
-// this is the image that will mask the initial one
-// photoHex2.image("./src/img/place-cat-b.jpeg").translate(IMAGE_OFFSET, IMAGE_OFFSET)
-// const catImageB = photoHex2
-//   .image("./src/img/place-cat-b.jpeg", 400, 200)
-//   .translate(IMAGE_OFFSET, IMAGE_OFFSET);
-
-photoHex2.use(hexagons);
+imageContainer.use(hexagons);
 
 const visibleMaskProps = {
   color: "#fff",
@@ -99,36 +91,40 @@ const visibleMaskProps = {
 
 // filter backing hexagon grid into a new list containing all hexagons
 // that fall within the base image
-const hexagonsInImage = backingGrid.reduce((memo, hexagon, index) => {
-  const { x, y } = hexagon.toPoint();
 
-  if (
-    catImageB.inside(x, y) ||
-    catImageB.inside(x - 22, y) ||
-    catImageB.inside(x + 22, y) ||
-    catImageB.inside(x + 22, y - 22) ||
-    catImageB.inside(x - 22, y + 22) ||
-    catImageB.inside(x, y - 22) ||
-    catImageB.inside(x, y + 22) ||
-    catImageB.inside(x + 22, y + 22)
-  ) {
-    memo = [
-      {
-        svg: hexagons.get(index),
-        memory: hexagon
-      },
-      ...memo
-    ];
-  }
+const getMaskOverlap = (grid, shapes, mask, offset = 0) => {
+  return grid.reduce((memo, polygon, index) => {
+    const { x, y } = polygon.toPoint();
 
-  return memo;
-}, []);
+    if (
+      mask.inside(x, y) ||
+      mask.inside(x - offset, y) ||
+      mask.inside(x + offset, y) ||
+      mask.inside(x + offset, y - offset) ||
+      mask.inside(x - offset, y + offset) ||
+      mask.inside(x, y - offset) ||
+      mask.inside(x, y + offset) ||
+      mask.inside(x + offset, y + offset)
+    ) {
+      memo = [
+        {
+          svg: shapes.get(index),
+          memory: polygon
+        },
+        ...memo
+      ];
+    }
 
-const hexImageMask = photoHex2.mask();
+    return memo;
+  }, []);
+};
+
+const hexagonsInImage = getMaskOverlap(backingGrid, hexagons, maskingImage);
+const hexImageMask = imageContainer.mask();
 const getRandomBetween = (min, max) => Math.floor(Math.random() * max) + min;
 const selected = {};
 const loopLength = hexagonsInImage.length;
-const time = 50;
+const time = 1000;
 let i = 0;
 
 const animationLoop = setInterval(() => {
@@ -155,17 +151,22 @@ const animationLoop = setInterval(() => {
       opacity: "1",
       color: "none"
     })
+    .stroke({ width: 1, color: "#000", opacity: "1" })
 
     .after(() => {
       svg
+        .stroke({ width: 1, color: "#000", opacity: "1" })
         .animate()
         .fill(visibleMaskProps)
+
         .after(() =>
           hexImageMask.add(
-            svg.translate(inMemoryGridPoint.x - 22, inMemoryGridPoint.y - 22)
+            svg
+              //.translate(inMemoryGridPoint.x - 22, inMemoryGridPoint.y - 22)
+              .stroke({ width: 1, color: "#000", opacity: "1" })
           )
         );
     });
-}, time);
+}, 500);
 
-catImageB.maskWith(hexImageMask);
+maskingImage.maskWith(hexImageMask).maskWith(hexagons);
